2a9b3bfbe1514256a71b1cf803ca6d367e5f643e
belalelmi <belalelmi@gmail.com>
2021-12-08 03:03:57 -0500

    moved the daylist component to the navbar

---
+++ src/components/DayList.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import DayListItem from './DayListItem'
+
+export default function DayList(props) {
+  return props.days.map(day => {
+    return (
+      <ul>
+        <DayListItem
+          key={day.id}
+          name={day.name}
+          spots={day.spots}
+          selected={day.name === props.day}
+          setDay={props.setDay}
+        />
+      </ul>
+    )
+  })
+}

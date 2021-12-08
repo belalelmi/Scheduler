27e43e34f411c9de3d3df81032d5e628502f2400
belalelmi <belalelmi@gmail.com>
2021-12-08 03:29:31 -0500

    fixed bug in App component -->  Daylist component

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,6 +5,8 @@ import "components/Application.scss";
 import DayList from "./DayList";
 
 
+
+
 const days = [
   {
     id: 1,
@@ -25,6 +27,7 @@ const days = [
 
 
 export default function Application(props) {
+  const [day, setDay] = useState("Monday")
   return (
     <main className="layout">
       <section className="sidebar">
@@ -37,8 +40,8 @@ export default function Application(props) {
         <nav className="sidebar__menu">
           <DayList
             days={days}
-            day={"Monday"}
-            setDay={day => console.log(day)}
+            day={day}
+            setDay={setDay}
           />
         </nav>
         <img

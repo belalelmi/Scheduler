4bc5cb1c5b4c41629367789dd030d18b753ac908
belalelmi <belalelmi@gmail.com>
2021-12-09 21:49:00 -0500

    appointment component complete

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
 
 import "components/Application.scss";
+import "components/Appointment"
 
 import DayList from "./DayList";
-
-
-
+import Appointment from "components/Appointment";
 
 const days = [
   {
@@ -24,6 +23,45 @@ const days = [
     spots: 0,
   },
 ];
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+  }
+];
+
 
 
 export default function Application(props) {
@@ -51,7 +89,9 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments.map(appointment => (
+          <Appointment key={appointment.id} {...appointment} />
+        ))}
       </section>
     </main>
   );
---
+++ src/components/Appointment/Form.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import Button from 'components/Button';
+import InterviewerList from 'components/InterviewerList';
+
+export default function Form(props) {
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("")
+  const reset = () => {
+    setStudent('')
+    setInterviewer(null)
+  }
+  const cancel = () => {
+    reset()
+    setError()
+    props.onCancel()
+  }
+  const validation = () => {
+    if (!student) {
+      setError('Student name cannot be blank');
+      return;
+    }
+    if (!interviewer) {
+      setError('Please select an interviewer');
+      return;
+    }
+    setError('');
+    props.onSave(student, interviewer);
+  }
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            value={student}
+            onChange={event => setStudent(event.target.value)}
+          />
+        </form>
+        <section className="appointment__validation">
+          {error}
+        </section>
+        <InterviewerList
+          interviewers={props.interviewers}
+          value={interviewer}
+          onChange={setInterviewer}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={cancel} >
+            Cancel</Button>
+          <Button confirm onClick={validation}>
+            Save</Button>
+        </section>
+      </section>
+    </main>
+  );
+}

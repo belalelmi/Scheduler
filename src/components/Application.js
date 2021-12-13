455369f9eea3c8ca936d5e2fc6748e789647336f
belalelmi <belalelmi@gmail.com>
2021-12-13 03:09:38 -0500

    adding interviewers selector

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "components/Application.scss";
-import { getAppointmentsForDay, getInterview } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
 import "components/Appointment";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
@@ -14,8 +14,11 @@ export default function Application(props) {
     appointments: {},
     interviewers: {}
   });
-  const dailyAppointments = getAppointmentsForDay(state, state.day);
   const setDay = day => setState({ ...state, day });
+
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+  const interviewers = getInterviewersForDay(state, state.day);
+  // console.log(interviewers)
 
 
   useEffect(() => {
@@ -45,6 +48,7 @@ export default function Application(props) {
         id={appointment.id}
         time={appointment.time}
         interview={interview}
+        interviewers={interviewers}
       />
     );
   });
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,7 +1,7 @@
 
 export function getAppointmentsForDay(state, day) {
   const matches = [];
-  const dayData = state.days.filter(d => d.name === day)
+  const dayData = state.days.filter(eachDay => eachDay.name === day)
 
   if (!dayData[0]) return matches;
   for (const appointment of dayData[0].appointments) {
@@ -16,8 +16,18 @@ export function getInterview(state, interview) {
     const interviewData = {
       student: interview.student,
       interviewer: { ...state.interviewers[interview.interviewer] }
-    }
+    };
     return interviewData;
-  }
+  };
   return null;
 }
+
+export function getInterviewersForDay(state, day) {
+  const result = []
+  const dayData = state.days.filter(eachDay => eachDay.name === day)
+  if (!dayData[0]) return result;
+  for (const interviwer of dayData[0].interviewers) {
+    result.push(state.interviewers[interviwer]);
+  }
+  return result;
+};

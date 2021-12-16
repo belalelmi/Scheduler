ae1efdac142b64652eb8538e8aa470a1b07ba297
belalelmi <belalelmi@gmail.com>
2021-12-16 06:12:22 -0500

    resolved error: Import in body of module

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-import "index.scss";
-
 import axios from "axios";
+import Application from "components/Application";
+import "index.scss";
 
 if (process.env.REACT_APP_API_BASE_URL) {
   axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
 }
-
-import Application from "components/Application";
 
 ReactDOM.render(<Application />, document.getElementById("root"));
